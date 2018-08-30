a8eeaf498fae21da71b8ce515602e390f784137d
satyasinha94 <satyasinha94@gmail.com>
2018-08-30 19:48:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,3 @@ function updateObjectWithKeyAndValue (object, key, value) {
   recipes[key] = value
   return recipes
 }
-function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
-
-}
