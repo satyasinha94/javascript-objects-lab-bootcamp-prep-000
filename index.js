89ab9a83e3bd77ddbcc258cfe07ea0f3c4b250b0
satyasinha94 <satyasinha94@gmail.com>
2018-08-30 20:37:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ function updateObjectWithKeyAndValue (object, key, value) {
 }
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   object[key] = value
-  return object 
+  return object
+}
+function deleteFromObjectByKey (object, key) {
+  Object.assign({}, object, {[key]})
 }
