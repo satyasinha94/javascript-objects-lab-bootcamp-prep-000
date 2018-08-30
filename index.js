8071c67ab56dad3b079782e95e65622296e03494
satyasinha94 <satyasinha94@gmail.com>
2018-08-30 21:00:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   return object
 }
 function deleteFromObjectByKey (object, key) {
-   Object.assign({}, object)
    return object
 }
 function destructivelyDeleteFromObjectByKey (object, key) {
