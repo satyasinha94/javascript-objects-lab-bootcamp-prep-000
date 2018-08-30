383bf2c434400eb3b833f6ab180a67046a884b94
satyasinha94 <satyasinha94@gmail.com>
2018-08-30 19:39:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 function updateObjectWithKeyAndValue (object, key, value) {
-  recipes[key] = 'value'
-  recipes[key] = 'value'
+  recipes[key] = '1'
+  recipes[key] = '2'
   return recipes
 }
