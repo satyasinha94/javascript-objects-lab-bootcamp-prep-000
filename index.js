c1d1579bede40ace76aa91dc3f3523a77509d406
satyasinha94 <satyasinha94@gmail.com>
2018-08-30 20:45:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function deleteFromObjectByKey (object, key) {
   return object
 }
 function destructivelyDeleteFromObjectByKey (object, key) {
-  delete object[key]
+  delete object[key];
   return object
 }
